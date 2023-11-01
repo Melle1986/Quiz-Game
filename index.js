2badeec01cfedbd0361c33d986f2e6d34972b28a
Melle1986 <melledom@googlemail.com>
2023-11-01 21:38:50 +0100

    play1

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const questions = [
         correctIndex: 1
     },
 
-    {   
+    {
         question: "Wie viele Bundesländer hat Deutschland?",
         options: ["14", "16", "18"],
         correctIndex: 1
@@ -53,57 +53,57 @@ function startTimer(seconds) {
 }
 
 function playSound() {
-    audioElement.play("audio");   //hier wird das audio element mit abgespielt 
+    audioElement.play();   //hier wird das audio element mit abgespielt 
 }
 
 
 startTimer(30);
 
-    const questionElement = document.getElementById("question");
-    const optionElements = document.querySelectorAll(".option");
-    const resultElement = document.getElementById("result");
-    const resultContainer = document.querySelector(".result-container");
+const questionElement = document.getElementById("question");
+const optionElements = document.querySelectorAll(".option");
+const resultElement = document.getElementById("result");
+const resultContainer = document.querySelector(".result-container");
 
-    let currentQuestionIndex = 0;
-    let score = 0;
+let currentQuestionIndex = 0;
+let score = 0;
 
-    function showQuestion() {
-        const question = questions[currentQuestionIndex];
-        questionElement.textContent = question.question;
+function showQuestion() {
+    const question = questions[currentQuestionIndex];
+    questionElement.textContent = question.question;
 
-        question.options.forEach((option, index) => {
-            optionElements[index].textContent = option;
-        });
-    }
-
-    function checkAnswer(selectedIndex) {
-        const question = questions[currentQuestionIndex];
-        if (selectedIndex === question.correctIndex) {
-            score++;
-        }
-        currentQuestionIndex++;
-
-        if (currentQuestionIndex < questions.length) {
-            showQuestion();
-        } else {
-            showResult();
-        }
-    }
-
-    function showResult() {
-        questionElement.textContent = "Quiz beendet!";
-        resultElement.textContent = `Ergebnis: ${score} von ${questions.length} Fragen richtig beantwortet.`;
-        resultContainer.style.display = "block";
-        optionElements.forEach((option) => {
-            option.style.display = "none";
-        });
-    }
-
-    optionElements.forEach((option, index) => {
-        option.addEventListener("click", () => {
-            checkAnswer(index);
-        });
+    question.options.forEach((option, index) => {
+        optionElements[index].textContent = option;
     });
+}
 
-    showQuestion();
+function checkAnswer(selectedIndex) {
+    const question = questions[currentQuestionIndex];
+    if (selectedIndex === question.correctIndex) {
+        score++;
+    }
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showResult();
+    }
+}
+
+function showResult() {
+    questionElement.textContent = "Quiz beendet!";
+    resultElement.textContent = `Ergebnis: ${score} von ${questions.length} Fragen richtig beantwortet.`;
+    resultContainer.style.display = "block";
+    optionElements.forEach((option) => {
+        option.style.display = "none";
+    });
+}
+
+optionElements.forEach((option, index) => {
+    option.addEventListener("click", () => {
+        checkAnswer(index);
+    });
+});
+
+showQuestion();
 
